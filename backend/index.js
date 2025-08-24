14402ca40f68b25555aa178de0b34147693128be
shani <shanikushwaha8121@gmail.com>
2025-08-25 01:30:33 +0530

    resolve cors error

--- backend/index.js
+++ backend/index.js
@@ -10,31 +10,33 @@ import paymentRouter from "./routes/paymentRoute.js";
 
 dotenv.config();
 
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
-        credentials: true
-    }
-));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://e-shiksha-v1vah1owm-shanisinghhs-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
-app.use("/api/auth",authRoute)
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/courses", courseRouter);
 app.use("/api/order", paymentRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello from backend");
+  res.send("Hello from backend");
 });
 
 app.listen(port, () => {
-    connectDB();
-    console.log("Server is running on port " + port);
+  connectDB();
+  console.log("Server is running on port " + port);
 });
